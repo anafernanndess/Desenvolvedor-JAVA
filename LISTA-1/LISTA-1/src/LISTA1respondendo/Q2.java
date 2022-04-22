951fc76138c39ac842a54e654ee78cd91567cf35
anafernanndess <analiuza123@gmail.com>
2022-04-22 20:52:37 -0300

    esse daqui é a atividade 2

---
+++ LISTA-1/LISTA-1/src/LISTA1respondendo/Q2.java
@@ -0,0 +1,8 @@
+package LISTA1respondendo;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int numero = 19;
+        System.out.println("O numero informado foi " + numero );
+    }
+}
