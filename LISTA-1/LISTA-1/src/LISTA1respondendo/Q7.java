5ea46826e35a97e0b38ab1b96b408f875efe9b60
anafernanndess <analiuza123@gmail.com>
2022-04-22 21:36:03 -0300

    esse daqui é a atividade 7

---
+++ LISTA-1/LISTA-1/src/LISTA1respondendo/Q7.java
@@ -0,0 +1,11 @@
+package LISTA1respondendo;
+
+public class Q7 {
+    public static void main(String[] args) {
+        //temperatura em Fº
+        double F = 80;
+        //mostrar a temperatura em Cº
+        double C = 5*((F-32)/9);
+        System.out.println(C);
+    }
+}
