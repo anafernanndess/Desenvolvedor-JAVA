1d50ef341283bba991d6ee3cd2f6aff8aff105c2
anafernanndess <analiuza123@gmail.com>
2022-04-22 21:43:45 -0300

    esse daqui é a atividade 8

---
+++ LISTA-1/LISTA-1/src/LISTA1respondendo/Q8.java
@@ -0,0 +1,9 @@
+package LISTA1respondendo;
+
+public class Q8 {
+    public static void main(String[] args) {
+        double C = 30;
+        double F = (C/5 * 9) + 32;
+        System.out.println(F);
+    }
+}
