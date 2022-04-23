59321f964e739463cc26d16c17b7ee1ee3a2366d
anafernanndess <analiuza123@gmail.com>
2022-04-22 21:03:28 -0300

    esse daqui é a atividade 3

---
+++ LISTA-1/LISTA-1/src/LISTA1respondendo/Q4.java
@@ -0,0 +1,12 @@
+package LISTA1respondendo;
+
+public class Q4 {
+    public static void main(String[] args) {
+        int nota1 = 5;
+        int nota2 = 10;
+        int nota3 = 8;
+        int nota4 = 2;
+        int media = (nota1 + nota2 + nota3 + nota4)/4;
+        System.out.println(media);
+    }
+}
