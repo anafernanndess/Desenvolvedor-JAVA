332640769923c424be8a3ede1c30f2abdb018b37
anafernanndess <90392277+anafernanndess@users.noreply.github.com>
2022-04-22 21:05:33 -0300

    Delete q3.java

--- LISTA-1/LISTA-1/src/LISTA1respondendo/q3.java
+++
@@ -1,9 +0,0 @@
-package LISTA1respondendo;
-
-public class q3 {
-    public static void main(String[] args) {
-        int numero = 8;
-        int numero2 = 8;
-        System.out.println(numero + numero2);
-    }
-}
