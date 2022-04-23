db69990e70974ea742bd0eb84630e462f561d093
anafernanndess <analiuza123@gmail.com>
2022-04-22 21:29:51 -0300

    esse daqui é a atividade 6

---
+++ LISTA-1/LISTA-1/src/LISTA1respondendo/Q6.java
@@ -0,0 +1,13 @@
+package LISTA1respondendo;
+
+public class Q6 {
+    public static void main(String[] args) {
+        //quanto vc ganha por hora
+        int pagamento_por_hora = 8;
+        //quantas horas vc trabalha por mes
+        int horas_por_mes = 240;
+        int calculo_do_salario = 240*8;
+        System.out.println(calculo_do_salario);
+
+    }
+}
