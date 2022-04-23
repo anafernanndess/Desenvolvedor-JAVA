34bdc7b2cd5bc95dd0ae80da7c5493cf0f758522
anafernanndess <analiuza123@gmail.com>
2022-04-22 21:18:47 -0300

    esse daqui é a atividade 5

---
+++ LISTA-1/LISTA-1/src/LISTA1respondendo/Q5.java
@@ -0,0 +1,11 @@
+package LISTA1respondendo;
+
+public class Q5 {
+    public static void main(String[] args) {
+        int lado1 = 5;
+        int lado2 = 5;
+        int area = lado1 * lado2;
+        System.out.println(area * 2);
+
+    }
+}
