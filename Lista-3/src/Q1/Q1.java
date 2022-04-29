b0fc1ca084c265a23d6e5bdb83387fc47410fbd0
anafernanndess <analiuza123@gmail.com>
2022-04-29 00:53:10 -0300

    q1

---
+++ Lista-3/src/Q1/Q1.java
@@ -0,0 +1,21 @@
+package Q1;
+import java.util.Scanner;
+import java.util.Locale;
+public class Q1 {
+    public class Array {
+        public void main(String[] args) {
+            Scanner leia = new Scanner(System.in);
+            final var ponto = new Locale("en", "us");
+            int [] numeros = new int[5];
+            int [] numero = {1,4,6,8,10};
+            System.out.println("numero do meu indice 2:" + numero[2]);
+            for(int i = 0; i < numero.length; i++) {
+                System.out.println("numero do indice" + i + ";" + numero [i]);
+                leia.close();
+            }
+        }
+    }
+}
+
+
+
